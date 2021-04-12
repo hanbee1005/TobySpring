50b30375816ae5f3ae79e082f43dbdd412bafca2
hanbee <sonhanbi1002@gmail.com>
2021-04-12 22:09:41 +0900

    DaoFactory 리팩토링

--- TobySpring/Chapter01/src/main/java/dao/DaoFactory.java
+++ TobySpring/Chapter01/src/main/java/dao/DaoFactory.java
@@ -2,7 +2,11 @@ package dao;
 
 public class DaoFactory {
     public UserDao userDao() {
-        ConnectionMaker connectionMaker = new DConnectionMaker();
-        return new UserDao(connectionMaker);
+        return new UserDao(getConnection());
+    }
+
+    // 추가로 다른 Dao들이 생겼을 때를 중복을 방지하기 위한 메소드 분리
+    public ConnectionMaker getConnection() {
+        return new DConnectionMaker();
     }
 }
