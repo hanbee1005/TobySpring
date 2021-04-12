1fa51a9b9aab6ced524f927af60a7f9bc2ada0a7
hanbee <sonhanbi1002@gmail.com>
2021-04-12 22:48:49 +0900

    스프링 관련 라이브러리 추가 및 DaoFactory를 설정파일로 변경 후 ApplicationContext에서 사용

--- TobySpring/Chapter01/src/main/java/Main.java
+++ TobySpring/Chapter01/src/main/java/Main.java
@@ -1,12 +1,15 @@
 import dao.DaoFactory;
 import dao.UserDao;
 import domain.User;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        UserDao dao = new DaoFactory().userDao();
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
         user.setId("whiteship");
--- TobySpring/Chapter01/src/main/java/dao/DaoFactory.java
+++ TobySpring/Chapter01/src/main/java/dao/DaoFactory.java
@@ -1,11 +1,17 @@
 package dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
+    @Bean
     public UserDao userDao() {
         return new UserDao(getConnection());
     }
 
     // 추가로 다른 Dao들이 생겼을 때를 중복을 방지하기 위한 메소드 분리
+    @Bean
     public ConnectionMaker getConnection() {
         return new DConnectionMaker();
     }
