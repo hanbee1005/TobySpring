5cc25a8796f555e3c72581392d29e8507e94b310
hanbee <sonhanbi1002@gmail.com>
2021-04-20 21:51:41 +0900

    set 객체를 만들어서 중복되는 객체가 만들어지지 않는지 확인하도록 코드 개선

--- Chapter02/src/test/java/JUnitTest.java
+++ Chapter02/src/test/java/JUnitTest.java
@@ -1,28 +1,29 @@
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.CoreMatchers.sameInstance;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
 
 public class JUnitTest {
-    static JUnitTest testObject;
+    static Set<JUnitTest> testObject = new HashSet<>();
 
     @Test
     public void test1() {
-        assertThat(this, is(not(sameInstance(testObject))));
-        testObject = this;
+        assertThat(testObject, not(hasItem(this)));
+        testObject.add(this);
     }
 
     @Test
     public void test2() {
-        assertThat(this, is(not(sameInstance(testObject))));
-        testObject = this;
+        assertThat(testObject, not(hasItem(this)));
+        testObject.add(this);
     }
 
     @Test
     public void test3() {
-        assertThat(this, is(not(sameInstance(testObject))));
-        testObject = this;
+        assertThat(testObject, not(hasItem(this)));
+        testObject.add(this);
     }
 }
