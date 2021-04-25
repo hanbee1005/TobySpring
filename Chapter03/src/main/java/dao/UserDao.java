68f6830d0c4753d7d41bd61d74e98851e73a95bf
hanbee <sonhanbi1002@gmail.com>
2021-04-25 21:44:52 +0900

    addStatement 추가

--- Chapter03/src/main/java/dao/UserDao.java
+++ Chapter03/src/main/java/dao/UserDao.java
@@ -13,22 +13,9 @@ public class UserDao {
         this.dataSource = dataSource;
     }
 
-    // JDBC API가 만들어내는 예외를 잡아서 직접 처리하거나, 메소드에 throws를 선언해서 예외가 발생하면 밖으로 던지게 한다.
     public void add(User user) throws SQLException {
-        Connection c = dataSource.getConnection();
-
-        // 2. SQL을 담은 Statement(또는 PreparedStatement)를 만든다.
-        PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
-        ps.setString(1, user.getId());
-        ps.setString(2, user.getName());
-        ps.setString(3, user.getPassword());
-
-        // 3. 만들어진 Statement를 실행한다.
-        ps.execute();
-
-        // 5. 작업 중에 생성된 Connection, Statement, ResultSet 같은 리소스는 작업을 마친 후 반드시 닫아준다.
-        ps.close();
-        c.close();
+        StatementStrategy st = new AddStatement(user);
+        jdbcContextWithStatementStrategy(st);
     }
 
     public User get(String id) throws SQLException {
