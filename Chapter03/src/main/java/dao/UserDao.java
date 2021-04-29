59109634671f9d0d83fbf30970ced8807ccded59
hanbee <sonhanbi1002@gmail.com>
2021-04-30 05:57:51 +0900

    콜백 분리하기

--- Chapter03/src/main/java/dao/UserDao.java
+++ Chapter03/src/main/java/dao/UserDao.java
@@ -58,13 +58,7 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-        this.jdbcContext.workWithStatementStrategy(new StatementStrategy() {
-            @Override
-            public PreparedStatement makePrepareStatement(Connection c) throws SQLException {
-                PreparedStatement ps = c.prepareStatement("delete from users");
-                return ps;
-            }
-        });
+        executeSql("delete from users");
     }
 
     public int getCount() throws SQLException {
@@ -104,5 +98,15 @@ public class UserDao {
                 }
             }
         }
+    }
+
+    private void executeSql(final String query) throws SQLException {
+        this.jdbcContext.workWithStatementStrategy(new StatementStrategy() {
+            @Override
+            public PreparedStatement makePrepareStatement(Connection c) throws SQLException {
+                PreparedStatement ps = c.prepareStatement(query);
+                return ps;
+            }
+        });
     }
 }
