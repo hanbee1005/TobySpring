6f6868bba6f640b0d913780e5bc1cc4bf77d0a8a
hanbee <sonhanbi1002@gmail.com>
2021-05-02 07:55:50 +0900

    getCount 메소드 일부 수정

--- Chapter03/src/main/java/dao/UserDao.java
+++ Chapter03/src/main/java/dao/UserDao.java
@@ -4,7 +4,6 @@ import domain.User;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -57,18 +56,14 @@ public class UserDao {
     }
 
     public int getCount() {
-        return this.jdbcTemplate.query(new PreparedStatementCreator() {
-            @Override
-            public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
-                return con.prepareStatement("select count(*) from users");
-            }
-        }, new ResultSetExtractor<Integer>() {
-            @Override
-            public Integer extractData(ResultSet rs) throws SQLException, DataAccessException {
-                rs.next();
-                return rs.getInt(1);
-            }
-        });
+        return this.jdbcTemplate.query("select count(*) from users",
+                new ResultSetExtractor<Integer>() {
+                    @Override
+                    public Integer extractData(ResultSet rs) throws SQLException, DataAccessException {
+                        rs.next();
+                        return rs.getInt(1);
+                    }
+                });
     }
 
     public List<User> getAll() {
