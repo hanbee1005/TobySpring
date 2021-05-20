07a2432482428160fee39e683bbaaed7d3676f7e
hanbee <sonhanbi1002@gmail.com>
2021-05-20 20:59:12 +0900

    chapter04 프로젝트 생성

---
+++ Chapter04/src/main/java/dao/JdbcContext.java
@@ -0,0 +1,45 @@
+package dao;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class JdbcContext {
+    private DataSource dataSource;
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void workWithStatementStrategy(StatementStrategy stmt) throws SQLException {
+        Connection c = null;
+        PreparedStatement ps = null;
+
+        try {
+            c = dataSource.getConnection();
+            ps = stmt.makePrepareStatement(c);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if (ps != null) { try { ps.close(); } catch (SQLException e) {} }
+            if (c != null) { try { c.close(); } catch (SQLException e) {} }
+        }
+    }
+
+    public void executeSql(final String query, final String ...params) throws SQLException {
+        workWithStatementStrategy(new StatementStrategy() {
+            @Override
+            public PreparedStatement makePrepareStatement(Connection c) throws SQLException {
+                PreparedStatement ps = c.prepareStatement(query);
+
+                for (int i = 0; i < params.length; i++) {
+                    ps.setString(i+1, params[i]);
+                }
+
+                return ps;
+            }
+        });
+    }
+}
