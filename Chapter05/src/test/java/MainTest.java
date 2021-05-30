3bf42687b0d25679475b5eb32e162f48e30b22d9
hanbee <sonhanbi1002@gmail.com>
2021-05-30 17:58:36 +0900

    update 메소드 생성 및 테스트

--- Chapter05/src/test/java/MainTest.java
+++ Chapter05/src/test/java/MainTest.java
@@ -97,6 +97,27 @@ class MainTest {
     }
 
     @Test
+    public void update() throws SQLException {
+        dao.deleteAll();
+
+        dao.add(user1);
+        dao.add(user2);
+
+        user1.setName("오민규");
+        user1.setPassword("springno6");
+        user1.setLevel(Level.GOLD);
+        user1.setLogin(1000);
+        user1.setRecommend(999);
+        dao.update(user1);
+
+        User user1update = dao.get(user1.getId());
+        checkSameUser(user1, user1update);
+
+        User user2same = dao.get(user2.getId());
+        checkSameUser(user2, user2same);
+    }
+
+    @Test
     @Disabled
     public void sqlExceptionTranslate() {
         dao.deleteAll();
