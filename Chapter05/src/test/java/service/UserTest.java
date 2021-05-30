50d4af2bda10fe0237adec4f3be65c946f1d4792
hanbee <sonhanbi1002@gmail.com>
2021-05-30 21:38:11 +0900

    user의 upgradeLevel 테스트

---
+++ Chapter05/src/test/java/service/UserTest.java
@@ -0,0 +1,42 @@
+package service;
+
+import domain.Level;
+import domain.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class UserTest {
+    User user;
+
+    @BeforeEach
+    public void setUp() {
+        user = new User();
+    }
+
+    @Test
+    public void upgradeLevel() {
+        Level[] levels = Level.values();
+        for (Level level : levels) {
+            if (level.nextLevel() == null) continue;
+            user.setLevel(level);
+            user.upgradeLevel();
+            assertThat(user.getLevel(), is(level.nextLevel()));
+        }
+    }
+
+    @Test
+    public void cannotUpgradeLevel() {
+        Level[] levels = Level.values();
+        for (Level level : levels) {
+            if (level.nextLevel() != null) continue;
+            user.setLevel(level);
+            assertThrows(IllegalStateException.class, () -> {
+                user.upgradeLevel();
+            });
+        }
+    }
+}
